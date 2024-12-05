74b3a9fbcae18af647592673776e525ce23436a1
renalsugiana <a22100107@mhs.stmik-sumedang.ac.id>
2024-12-06 06:05:48 +0700

    update config

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:18.19.1
 WORKDIR /app
-ENV PORT 3000
+ENV PORT 8080
 ENV MODEL_URL 'https://storage.googleapis.com/model-mlgc2024/model.json'
 COPY . .
 RUN npm install
-EXPOSE 3000
+EXPOSE 8080
 CMD [ "npm", "run", "start"]
--- src/server/server.js
+++ src/server/server.js
@@ -6,8 +6,8 @@ const InputError = require('../exceptions/InputError');
 
 (async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 3000,
-    host: 'localhost',
+    port: process.env.PORT || 8080,
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
